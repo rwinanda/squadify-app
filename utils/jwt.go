5aeb19fa69bf15c0589321cfd630e06eca73d5c7
Mochammad Winanda <mochammadrwinanda@gmail.com>
2025-09-22 14:37:48 +0700

    feat: "JWT Secret Key"
    - move JWT key to .env

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,12 +1,13 @@
 package utils
 
 import (
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secretKey = []byte("secret-key")
+var secretKey = []byte(os.Getenv("JWT_KEY"))
 
 func CreateJWT(id uint) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
@@ -14,7 +15,9 @@ func CreateJWT(id uint) (string, error) {
 			"id":  id,
 			"exp": time.Now().Add(time.Hour * 24).Unix(),
 		})
+
 	tokenString, err := token.SignedString(secretKey)
+
 	if err != nil {
 		return "", err
 	}
